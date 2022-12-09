abb347ce0aa6466d8983d8aae9bb73ba56fbdaff
samamanikantareddy <manikantasama4@gmail.com>
2022-12-09 16:26:32 +0100

    Add test project with required tests

---
+++ LibraryTests/CatAPITest.cs
@@ -0,0 +1,27 @@
+using Library;
+
+namespace LibraryTests
+{
+    class TestRecord
+    {
+        public string? field { get; set; }
+    }
+
+    [TestClass]
+    public class CatAPITest
+    {
+        [TestMethod]
+        public void TestApiFetch()
+        {
+            var api = new CatApiClient<TestRecord>();
+            var response = @"
+                            [{'field': 'value1'}, {'field': 'value2'}]
+                            ";
+            List<TestRecord>? records = api.CreateCatsFromJson(response);
+            Assert.IsNotNull(records);
+            Assert.AreEqual(2, records.Count());
+            Assert.AreEqual("value1", records[0].field);
+            Assert.AreEqual("value2", records[1].field);
+        }
+    }
+}
