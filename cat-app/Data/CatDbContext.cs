b5713fc64bbd681bfdd7a5351d5f7ec7ceb592cb
samamanikantareddy <manikantasama4@gmail.com>
2022-12-09 10:31:02 +0100

    Create sqlite db with identity tables

---
+++ cat-app/Data/CatDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace cat_app.Data
+{
+    public class CatDbContext: IdentityDbContext
+    {
+        public CatDbContext(DbContextOptions options): base(options)
+        {
+        }
+    }
+}
--- cat-app/Program.cs
+++ cat-app/Program.cs
@@ -1,7 +1,13 @@
+using cat_app.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// add sqlite database
+builder.Services.AddDbContext<CatDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
